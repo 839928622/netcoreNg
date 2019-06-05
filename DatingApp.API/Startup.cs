14527b036f052f4ef47a4964bb78554af6abf8d4
Alex <839928622@qq.comm>
2019-06-05 23:38:39 +0800

    User management and photo management added

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -37,48 +37,103 @@ namespace DatingApp.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // public void ConfigureServices(IServiceCollection services)
-        // {
-        //     services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
-        //     .AddJsonOptions(options => {
-        //       options.SerializerSettings.ReferenceLoopHandling = 
-        //               Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-        //     });
+       // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+            // .AddJsonOptions(options => {
+            //   options.SerializerSettings.ReferenceLoopHandling = 
+            //           Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            // });
 
-        //     services.AddDbContext<DataContext>(d =>d.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
-        //     .ConfigureWarnings(warnings => warnings.Ignore(CoreEventId.IncludeIgnoredWarning))
-        //     );//这里可以使用上面注入的IConfiguration Configration ，Configuration对应的是appsettings.json这个json文件，里面可以获取到我们写入的配置信息。
-        //     services.BuildServiceProvider().GetService<DataContext>().Database.Migrate(); // 这个命令会在运行的时候，执行未经处理的迁移migtaions。没有数据库的创建数据库，并应用迁移到数据库。有了数据库且已经迁移则无影响。 这个是自动化部署的内容之一。因为我们无法在本地使用dotnet ef migrations add [迁移的名字] 然后dotnet ef database uodate 。这样是行不通的。因为远程的容器/数据库不允许这么做。如果远程允许。那么上面的命令可以不需要，然后手动更新表结构至远程。
-        //     services.AddCors(); //允许夸源访问
-        //     services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings")); //绑定json中的设置到 类中的设置，然后CloudinarySettings类中的属性值将匹配至json中配置的值
-        //     services.AddAutoMapper();
-        //     services.AddTransient<Seed>();
-        //     services.AddScoped<IAuthRepository,AuthRepository>(); // 对于注入服务的这种理解需要加强 目前还不了解
-        //     services.AddScoped<IDatingRepository,DatingRepository>();
+            services.AddDbContext<DataContext>(d =>d.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            .ConfigureWarnings(warnings => warnings.Ignore(CoreEventId.IncludeIgnoredWarning))
+            );//这里可以使用上面注入的IConfiguration Configration ，Configuration对应的是appsettings.json这个json文件，里面可以获取到我们写入的配置信息。
+            services.BuildServiceProvider().GetService<DataContext>().Database.Migrate(); // 这个命令会在运行的时候，执行未经处理的迁移migtaions。没有数据库的创建数据库，并应用迁移到数据库。有了数据库且已经迁移则无影响。 这个是自动化部署的内容之一。因为我们无法在本地使用dotnet ef migrations add [迁移的名字] 然后dotnet ef database uodate 。这样是行不通的。因为远程的容器/数据库不允许这么做。如果远程允许。那么上面的命令可以不需要，然后手动更新表结构至远程。
+            IdentityBuilder builder = services.AddIdentityCore<User>(options=>
+            {
+             // options.Password.RequireDigit = true;
+              options.Password.RequiredLength = 9 ;
+            //  options.Password.RequireNonAlphanumeric = true; // 需要非字符数字 的符号
+              //options.Password.RequireLowercase = true;
+             // options.Password.RequireUppercase = true;
+             
+
+            });
+
+            builder = new IdentityBuilder(builder.UserType, typeof(Role), builder.Services);//
+            builder.AddEntityFrameworkStores<DataContext>(); // 告诉identtiy 我们想要使用ef作为存储
+            builder.AddRoleValidator<RoleValidator<Role>>();
+            builder.AddRoleManager<RoleManager<Role>>();
+            builder.AddSignInManager<SignInManager<User>>();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options=>{
+             options.TokenValidationParameters = new TokenValidationParameters
+             {
+                ValidateIssuerSigningKey = true,
+                 IssuerSigningKey = new SymmetricSecurityKey(
+                     Encoding
+                     .ASCII
+                     .GetBytes(Configuration.GetSection("AppSettings:Token").Value)), // 由于我们的key是字符串，因此要转换为byte[] 
+                    ValidateIssuer = false, // 由于是本地项目 设置为false
+                    ValidateAudience = false // localhost 
+
+             };
+
+
+            });
+
+            services.AddAuthorization(options => { // 授权
+            options.AddPolicy("RequireAdmin",policy=>policy.RequireRole("Admin"));
+
+               options.AddPolicy("ModeratePhotoRole",policy=>policy.RequireRole("Admin","Moderator"));
+
+                  options.AddPolicy("VipOnly",policy=>policy.RequireRole("VIP"));
+
+            });
+            services.AddMvc(options =>
+            {
+              var policy = new AuthorizationPolicyBuilder()
+                             .RequireAuthenticatedUser()
+                             .Build();// 全局鉴权
+                options.Filters.Add(new AuthorizeFilter(policy));
+                
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+            .AddJsonOptions(options => {
+              options.SerializerSettings.ReferenceLoopHandling = 
+                      Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
+           
+           
+            services.AddCors(); //允许夸源访问
+            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings")); //绑定json中的设置到 类中的设置，然后CloudinarySettings类中的属性值将匹配至json中配置的值
+            services.AddAutoMapper();
+            services.AddTransient<Seed>();
+            services.AddScoped<IAuthRepository,AuthRepository>(); // 对于注入服务的这种理解需要加强 目前还不了解
+            services.AddScoped<IDatingRepository,DatingRepository>();
             
-        //     // 这里是告诉 mvc 我们使用何种Authentication
-        //     services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-        //     .AddJwtBearer(options=>{
-        //      options.TokenValidationParameters = new TokenValidationParameters
-        //      {
-        //         ValidateIssuerSigningKey = true,
-        //          IssuerSigningKey = new SymmetricSecurityKey(
-        //              Encoding
-        //              .ASCII
-        //              .GetBytes(Configuration.GetSection("AppSettings:Token").Value)), // 由于我们的key是字符串，因此要转换为byte[] 
-        //             ValidateIssuer = false, // 由于是本地项目 设置为false
-        //             ValidateAudience = false // localhost 
+            // 这里是告诉 mvc 我们使用何种Authentication
+            // services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            // .AddJwtBearer(options=>{
+            //  options.TokenValidationParameters = new TokenValidationParameters
+            //  {
+            //     ValidateIssuerSigningKey = true,
+            //      IssuerSigningKey = new SymmetricSecurityKey(
+            //          Encoding
+            //          .ASCII
+            //          .GetBytes(Configuration.GetSection("AppSettings:Token").Value)), // 由于我们的key是字符串，因此要转换为byte[] 
+            //         ValidateIssuer = false, // 由于是本地项目 设置为false
+            //         ValidateAudience = false // localhost 
 
-        //      };
+            //  };
 
-        //     });
+            // });
 
-        //     services.AddScoped<LogUserActivity>();
-        // }
+            services.AddScoped<LogUserActivity>();
+        }
 
 
-         public void ConfigureServices(IServiceCollection services)
+         public void ConfigureDevelopmentServices(IServiceCollection services)
         {
 
 
